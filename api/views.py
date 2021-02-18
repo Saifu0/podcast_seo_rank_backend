3b799a1b62205b1755a05ac64be0c07b9ab353d4
Saifur Rehman Khan <saifurrahmankhankhan@gmail.com>
2021-02-18 15:28:42 +0530

    top 20 countries

--- api/views.py
+++ api/views.py
@@ -4,53 +4,69 @@ import requests
 from rest_framework.response import Response
 from django.http import HttpResponse
 import time
+import os
+from operator import itemgetter
+
+
+def GetAllCountries():
+    try:
+        filename = open(os.getcwd()+"/api/countries.json",)
+    except:
+        print(os.getcwd())
+
+    countries = json.load(filename)
+    filename.close()
+    return countries
 
 
 def AsyncTask(keyword, podcast_id):
-    countries = ["us", "ca", "sg", "in", "cn", "au", "ue"]
+    # countries = ["us", "ca", "sg", "in", "cn", "au", "ue"]
+    countries = GetAllCountries()
 
     res = []
     urls = []
     for country in countries:
-        url = f"https://itunes.apple.com/search?term={keyword}&entity=podcast&country={country}"
+        url = f"https://itunes.apple.com/search?term={keyword}&entity=podcast&country={countries[country]}"
         urls.append(url)
 
-    for i in range(len(countries)):
-        country = countries[i]
+    i = 0
+    for country in countries:
         url = urls[i]
+        i += 1
         try:
             rank = 100000
             r = requests.get(url)
             r = json.loads(r.content)
             results = r["results"]
             n = len(results)
+            ok = 0
             for idx in range(n):
-                # print(type(results[idx]["trackId"]), type(podcast_id))
                 if results[idx]["trackId"] == podcast_id:
                     res.append({"country": country, "search_rank": idx+1})
-                    # rank = idx+1
+                    ok = 1
                     break
+            if ok == 0:
+                res.append({"country": country, "search_rank": rank})
         except:
             rank = 100000
             res.append({"country": country, "search_rank": rank})
 
-    return res
+    newlist = sorted(res, key=itemgetter("search_rank"))
+    response = []
+
+    for i in range(20):
+        response.append(newlist[i])
+
+    return response
 
 
 def SeoReportView(request):
     if request.method == "GET":
-        # print(request.body)
-        # json_data = json.loads(request.body)
-        # keyword = json_data['keyword']
-        # podcast_id = json_data['podcast_id']
 
         keyword = request.GET.get('keyword')
         podcast_id = request.GET.get('podcast_id')
-        # time.sleep(5)
 
         podcast_id = (int)(podcast_id)
-        # print('[KEYWORD]', keyword)
-        # print('[PODCAST_ID]', type(podcast_id))
 
         res = AsyncTask(keyword, podcast_id)
 
